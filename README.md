2f0a8db8dad21e269009fb2cc603af949ec12c48
Patrick Huesler <patrick.huesler@gmail.com>
2012-10-04 18:24:52 +0200

    get hello world

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Hopefully, when it's grown up, this will be an Erlang web app starter kit.
     $ rebar create-app srvid=aloha
     $ cp templates/rebar.config.template rebar.config
     $ rebar get-deps compile
+	$ cp templates/aloha_api.erl aloha_api.erl
+	$ cp templates/aloha_app.erl aloah_app.erl
+	$ rebar compile
+	$ erl -pa deps/*/ebin ebin -s aloha_app
+	$ curl localhost:3000/aloha
---
+++ templates/aloha_api.erl
@@ -0,0 +1,29 @@
+-module(aloha_api).
+-export([handle/2, handle_event/3]).
+-include_lib("elli/include/elli.hrl").
+-include("elli.hrl").
+-behaviour(elli_handler).
+
+
+%%
+%% ELLI REQUEST CALLBACK
+%%
+handle(Req, _Args) ->
+    %% Delegate to our handler function
+    handle(Req#req.method, elli_request:path(Req), Req).
+
+%% Route METHOD & PATH to the appropriate clause
+handle('GET',[<<"aloha">>], _Req) ->
+    {ok, [], <<"kekahi mau pipi">>};
+
+handle(_, _, _Req) ->
+    {404, [], <<"wipe out!">>}.
+
+%%
+%% ELLI EVENT CALLBACKS
+%%
+
+%% @doc: Handle request events, like request completed, exception
+%% thrown, client timeout, etc. Must return 'ok'.
+handle_event(_Event, _Data, _Args) ->
+    ok.
