b09bfed8122f389beb62aeb8235e2e3878b9bbc6
Patrick Huesler <patrick.huesler@gmail.com>
2012-10-08 16:13:07 -1000

    add dev start function

--- src/aloha_app.erl
+++ src/aloha_app.erl
@@ -13,9 +13,7 @@ start() ->
     application:start(aloha, permanent).
 
 start(_StartType, _StartArgs) ->
-    ok = application:start(compiler),
-    ok = application:start(syntax_tools),
-    ok = application:start(lager),
+    ok = dev_start(lager),
     reloader:start(),
     {ok, DefaultPort} = application:get_env(aloha, worker_port),
     Port = get_port(DefaultPort),
@@ -31,4 +29,15 @@ get_port(Default) ->
     case os:getenv(Key) of
         false -> Default;
         Val -> list_to_integer(Val)
+    end.
+
+dev_start(App) -> dev_start(App, temporary).
+
+dev_start(App, Type) ->
+    case application:start(App, Type) of
+        {error, {not_started, DepApp}} ->
+            dev_start(DepApp),
+            dev_start(App, Type);
+        ok -> ok;
+        {error, {already_started, App}} -> ok
     end.
