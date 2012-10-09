e69a912e95dcfc6784750b398e10f0df7d143939
Patrick Huesler <patrick.huesler@gmail.com>
2012-10-08 14:23:36 -1000

    adding etest skeleton

--- test/aloha_utils_test.erl
+++ test/aloha_utils_test.erl
@@ -4,5 +4,21 @@
 % Include etest's assertion macros.
 -include_lib("etest/include/etest.hrl").
 
+before_suite() ->
+    %start up applications
+    ok.
+
+before_test() ->
+    %load fixtures
+    ok.
+
 test_hello() ->
     ?assert_equal("kekahi mau pipi", aloha_utils:hello()).
+
+after_test() ->
+    % tear down fixtures
+    ok.
+
+after_suite() ->
+    % stop applications
+    ok.
