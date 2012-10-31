98ac375ba6d1bb104bf58a4dfc4c63f4597a8bc4
Patrick Huesler <patrick.huesler@gmail.com>
2012-10-31 16:03:49 +0100

    see if it still works

--- src/aloha_api.erl
+++ src/aloha_api.erl
@@ -14,6 +14,7 @@ handle(Req, _Args) ->
 
 %% Route METHOD & PATH to the appropriate clause
 handle('GET',[<<"aloha">>], _Req) ->
+    lager:debug("what is wrong"),
     {ok, [], <<"kekahi mau pipi">>};
 
 handle('GET',[<<"log">>], _Req) ->
